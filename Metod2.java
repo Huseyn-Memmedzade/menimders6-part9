dc059dea4c2f7af286545d63f693cf8ee9093b6f
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-07-27 19:39:47 +0400

    Add files via upload

---
+++ Metod2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Metod2 {
+    public static void main(String[] args) {
+
+        metod2();
+
+
+    }
+
+    public static void metod2() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zehmet Olmasa Ededi Daxil Edin");
+        int eded = sc.nextInt();
+
+        if (eded % 2 == 0) {
+            System.out.println("Eded Cuttur");
+
+        } else {
+            System.out.println("Eded Tekdir");
+        }
+    }
+
+}
+
+// Əhsən, Huseyn! 👏
+//Sənin metod2() metodun tamamilə doğrudur və çox gözəl şəkildə yazılıb! Bu, tək/cüt yoxlanışı üçün ideal nümunədir.
